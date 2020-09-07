a8fb81fbfd2012eb3b908e02b81c4d34068d358e
Erika <etbono96@gmail.com>
2020-09-07 17:23:05 -0300

    first commit

---
+++ js/carrossel-produtos.js
@@ -0,0 +1,41 @@
+(function () {
+
+    if (!window.matchMedia('(max-width: 770px)').matches) {
+
+        var produtos = document.querySelectorAll('.produto');
+
+        produtos.forEach(function (produto) {
+            produto.classList.toggle("oculto");
+        });
+
+        var exibido = 0;
+
+        produtos[exibido].classList.toggle("oculto");
+
+        var setaDireita = document.querySelector('#seta-carrossel-dir');
+
+        setaDireita.addEventListener("click", function () {
+            produtos[exibido].classList.toggle("oculto");
+            exibido++;
+
+            if (exibido >= produtos.length) {
+                exibido = 0;
+            }
+
+            produtos[exibido].classList.toggle("oculto");
+        });
+
+        var setaEsquerda = document.querySelector('#seta-carrossel-esq');
+
+        setaEsquerda.addEventListener("click", function () {
+            produtos[exibido].classList.toggle("oculto");
+            exibido--;
+
+            if (exibido < 0) {
+                exibido = produtos.length - 1;
+            }
+
+            produtos[exibido].classList.toggle("oculto");
+        });
+    }
+}());
